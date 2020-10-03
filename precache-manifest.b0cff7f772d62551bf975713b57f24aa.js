075571e11e4523063b38be8f4fb01db89f470d3c
Edgardo Rios <edgardo.rios@protonmail.com>
2020-10-03 15:25:20 -0500

    Updates

---
+++ precache-manifest.b0cff7f772d62551bf975713b57f24aa.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "049c42bf75a6ef0a886d6539dc72585f",
+    "url": "/markdown-previewer/index.html"
+  },
+  {
+    "revision": "b2cd7142413fcfe10c12",
+    "url": "/markdown-previewer/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "f3b6a7a2cec7a4b57414",
+    "url": "/markdown-previewer/static/css/main.3cd19234.chunk.css"
+  },
+  {
+    "revision": "b2cd7142413fcfe10c12",
+    "url": "/markdown-previewer/static/js/2.44c60618.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/markdown-previewer/static/js/2.44c60618.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f3b6a7a2cec7a4b57414",
+    "url": "/markdown-previewer/static/js/main.efa948df.chunk.js"
+  },
+  {
+    "revision": "380a9cf99d31db8da525",
+    "url": "/markdown-previewer/static/js/runtime-main.4fd0b226.js"
+  }
+]);
